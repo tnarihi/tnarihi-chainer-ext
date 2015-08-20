54833820c6169987946e9c94c5110b6bc0ac15ae
Takuya Narihira <dvbscb@gmail.com>
2015-08-20 15:54:53 -0700

    WrappedFunctions support save/load to HDF5

--- tnarihi_chainer_ext/wrapped_functions.py
+++ tnarihi_chainer_ext/wrapped_functions.py
@@ -1,6 +1,43 @@
 from future.builtins import super
 
+import numpy as np
+
 from chainer import function
+from chainer.cuda import to_cpu
+
+try:
+    import h5py
+    h5py_enabled = True
+except ImportError:
+    h5py_enabled = False
+
+
+def _force_to_cpu(x):
+    if isinstance(x, np.ndarray):
+        return x
+    return to_cpu(x)
+
+
+def _copy_param(src, dst):
+    if dst.size != src.size:
+        # use size rather than shape since h5py store shape with ndim > 1
+        print(
+            'Shapes mismatch: src={}, dst={}. Skip'.format(
+                src.shape, dst.shape))
+        return
+    if isinstance(dst, np.ndarray):
+        dst.flat[:] = src.flat
+    else:
+        dst.get(src.reshape(dst.shape))
+
+
+def _parameter_generator(func):
+    if hasattr(func, 'parameter_names'):
+        for paramname in func.parameter_names:
+            yield paramname, getattr(func, paramname)
+    else:
+        for pind, param in func.parameters:
+            yield str(pind), param
 
 
 class WrappedFunctions(function.Function):
@@ -38,3 +75,74 @@ class WrappedFunctions(function.Function):
     @gradients.setter
     def gradients(self, grads):
         self.f.gradients = grads
+
+    def save_parameters_hdf5(self, filename=None, h5mode='w', h5d=None):
+        """
+        Args:
+            filename (str): filename of HDF5 file to be created or overwritten
+            h5mode (str): HDF5 file mode
+            h5d (HDF5 file descriptor): Used only in recursively call
+        """
+        assert h5py_enabled, "Install h5py."
+        assert filename is None or h5d is None, \
+            'Both of filename and h5d should not be provided.'
+        assert filename is not None or h5d is not None, \
+            'Either of filename or h5d should be provided.'
+
+        def _core(h5d):
+            for funcname, func in self.f._get_sorted_funcs():
+                h5d_child = h5d.create_group(funcname)
+                if isinstance(func, WrappedFunctions):
+                    func.save_parameters_hdf5(h5d=h5d_child)
+                else:
+                    for paramname, param in _parameter_generator(func):
+                        print('Saving parameter: {}/{}'.format(
+                            h5d_child.name, paramname))
+                        h5d_child[paramname] = _force_to_cpu(param)
+        if h5d is None:
+            print ('Saving hdf5 file: {}'.format(filename))
+            with h5py.File(filename, h5mode) as h5d:
+                _core(h5d)
+        else:
+            print ('Saving function: {}'.format(h5d.name))
+            _core(h5d)
+
+    def load_parameters_hdf5(self, filename=None, h5d=None):
+        """
+        Args:
+            filename (str): filename of HDF5 file
+            h5d (HDF5 file descriptor): Used only in recursively call
+        """
+        assert h5py_enabled, "Install h5py."
+        assert filename is None or h5d is None, \
+            'Both of filename and h5d should not be provided.'
+        assert filename is not None or h5d is not None, \
+            'Either of filename or h5d should be provided.'
+
+        def _core(h5d):
+            for funcname, func in self.f._get_sorted_funcs():
+                try:
+                    h5d_child = h5d[funcname]
+                except KeyError:
+                    print("Parameters of {}/{} doesn't exist. Skip.".format(
+                        h5d.name, funcname))
+                    continue
+                if isinstance(func, WrappedFunctions):
+                    func.load_parameters_hdf5(h5d=h5d_child)
+                else:
+                    for paramname, dst in _parameter_generator(func):
+                        print('Loading parameter: {}/{}'.format(
+                            h5d_child.name, paramname))
+                        try:
+                            src = h5d_child[paramname].value
+                        except KeyError:
+                            print ("Parameter doesn't exist. Skip.")
+                            continue
+                        _copy_param(src, dst)
+        if h5d is None:
+            print ('Loading hdf5 file: {}'.format(filename))
+            with h5py.File(filename, 'r') as h5d:
+                _core(h5d)
+        else:
+            print ('Loading function: {}'.format(h5d.name))
+            _core(h5d)
